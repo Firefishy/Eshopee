81de179181929aea4722e9c4f2c241d486054b4d
salomo07 <kenjibanzai@gmail.com>
2018-07-25 20:18:27 +0700

    XXX

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -10,6 +10,5 @@
 <link href="assets/css/toastr.min.css" rel="stylesheet"/>
 <link rel="icon" href="http://www.kenji-martialarts.com/cni-content/uploads/modules/siteconfig/favicon.ico" type="image/x-icon" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="E-Commerce" />
 <script src="assets/js/jquery.min.js"></script>
