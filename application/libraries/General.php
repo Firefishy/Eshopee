d1c5dced89859314f8e0a8d37704675056beeb65
salomo07 <kenjibanzai@gmail.com>
2018-07-25 16:58:51 +0700

    XXX

--- application/libraries/General.php
+++ application/libraries/General.php
@@ -6,6 +6,7 @@ class General {
     {
         $this->CI =& get_instance();
         $this->CI->load->model('Mdefault');
+        echo base_url();
         header('Access-Control-Allow-Origin: *');
     }
     function checkLogged()
