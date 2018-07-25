252ac3287b89335a9d669db7c064c4f2cd7fb6e4
salomo07 <kenjibanzai@gmail.com>
2018-07-25 14:40:47 +0700

    XXX

--- application/models/Mdefault.php
+++ application/models/Mdefault.php
@@ -3,7 +3,8 @@
 class Mdefault extends CI_Model {
     function __construct() {
         parent::__construct();
-        $connection = new MongoDB\Client("mongodb://".HOSTNAME_MONGO.":".PORT_MONGO);
+        $connection = new MongoDB\Client("mongodb://".USERNAME_MONGO.":".PASSWORD_MONGO."@".HOSTNAME_MONGO.":".PORT_MONGO);
+        //$connection = new MongoDB\Client("mongodb://".HOSTNAME_MONGO.":".PORT_MONGO);
         $this->kenjishop = $connection->kenjishop;
     }
     function getPromo($find,$sort)
