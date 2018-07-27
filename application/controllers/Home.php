eacb740610c98db71547aa942d253603a06e1f1f
salomo07 <kenjibanzai@gmail.com>
2018-07-27 15:37:33 +0700

    xxx

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		echo base_url();
-		echo "\n".$_SERVER['HTTPS'];
+		echo "\n".$_SERVER['HTTP'];
 		$data['loggedin']=$this->loggedin;
 		$data['promo']=$this->Mdefault->getPromo(['IsActive'=>true],[]);
 		$data['categories']=$this->Mdefault->getCategories([],['limit'=>4,'sort'=>['Createdtime'=>1]]);
