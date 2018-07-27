0e23f11ba8cfcb1efcdd90f2f742772618c03be5
salomo07 <kenjibanzai@gmail.com>
2018-07-27 15:02:55 +0700

    xxx

--- application/config/config.php
+++ application/config/config.php
@@ -4,9 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 //$config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];//'https://eshopee.herokuapp.com/';
 
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$protocol = (!empty($_SERVER['HTTPS'])) ? 'https' : 'http';
+$base_url = $protocol . '://mysite.com';
 
 $config['index_page'] = '';
 $config['uri_protocol']	= 'AUTO';
